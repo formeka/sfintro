5cb2e45b1b32be9dff587267e73cfd3f9708c2e4
Mehdi Key <formk@kermed.fr>
2024-01-03 16:52:27 +0100

    Ajout de route dans les controllers

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,11 +2,20 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController 
 {
+    #[Route('/')]
     public function index(): Response
     {
         return new Response('Bonne année !');
+    }
+
+    #[Route('/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        return new Response($number);
     }
 }
